73395c74395414334f8f6b3527cca3d6404c84c6
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-08-08 17:59:13 -0300

    buscando e listando clientes

--- src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
+++ src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ServicoPrestadoBusca} from "./servicoPrestadoBusca";
+import {ServicoPrestadoService} from "../../servico-prestado.service";
 
 @Component({
   selector: 'app-servico-prestado-lista',
@@ -10,7 +12,11 @@ export class ServicoPrestadoListaComponent implements OnInit {
   nome: string;
   mes: number;
   meses: number[];
-  constructor() {
+  listaServicoPrestado: ServicoPrestadoBusca[];
+  message: string;
+  constructor(
+      private service: ServicoPrestadoService
+  ) {
     this.meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   }
 
@@ -18,8 +24,14 @@ export class ServicoPrestadoListaComponent implements OnInit {
   }
 
   consultar(){
-    console.log(this.nome);
-    console.log(this.mes);
+    this.service.buscar(this.nome, this.mes)
+        .subscribe( response => {
+          this.listaServicoPrestado = response;
+          if (this.listaServicoPrestado.length <= 0) {
+            this.message = 'Nenhum registro encontrado';
+          }else{
+            this.message = null;
+          }
+        });
   }
-
 }
